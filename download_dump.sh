f8ae7d018703d3df6d246323760d051984b8b813
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2022-11-13 13:46:41 +0100

    bash vars

--- download_dump.sh
+++ download_dump.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Common utils
+wget_cmd="wget -q --show-progress --limit-rate=10M"
+out_dir=$(dirname -- "$0")/dump
 
-wget -q --limit-rate=10M --show-progress https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2 -O $(dirname -- "$0")/dump/enwiki-pages-articles-multistream.xml.bz2
-wget -q --limit-rate=10M --show-progress https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream-index.txt.bz2 -O $(dirname -- "$0")/dump/enwiki-pages-articles-multistream-index.txt.bz2
+$wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2 -O $out_dir/enwiki-pages-articles-multistream.xml.bz2
+$wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream-index.txt.bz2 -O $out_dir/enwiki-pages-articles-multistream-index.txt.bz2
 
