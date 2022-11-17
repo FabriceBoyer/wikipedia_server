f5404d736c7c844f9bfb5de3604d1244e19ff488
Fabrice Boyer <fabrice.boyer@easymile.com>
2022-11-17 08:30:15 +0100

    mkdir

--- download_dump.sh
+++ download_dump.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Common utils
 wget_cmd="wget -q --show-progress --limit-rate=10M"
+
 out_dir=$(dirname -- "$0")/dump
+mkdir $out_dir
 
 $wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2 -O $out_dir/enwiki-pages-articles-multistream.xml.bz2
 $wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream-index.txt.bz2 -O $out_dir/enwiki-pages-articles-multistream-index.txt.bz2
