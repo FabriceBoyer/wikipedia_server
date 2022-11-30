e7e23df7fd14f99c549d793602789fa0806e2ebf
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2022-11-30 20:52:33 +0100

    wiktionary

--- download_dump.sh
+++ download_dump.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Common utils
-wget_cmd="wget -q --show-progress --limit-rate=10M"
+wget_cmd="wget -q -c --show-progress --limit-rate=1M"
 
 source $(dirname -- "$0")/.env
 out_dir=$DUMP_PATH
 mkdir $out_dir
 
+# Wikipedia
 $wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2 -O $out_dir/enwiki-pages-articles-multistream.xml.bz2
 $wget_cmd https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream-index.txt.bz2 -O $out_dir/enwiki-pages-articles-multistream-index.txt.bz2
 
+# wiktionary
+$wget_cmd https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles-multistream.xml.bz2 -O $out_dir/enwiktionary-pages-articles-multistream.xml.bz2
+$wget_cmd https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles-multistream-index.txt.bz2 -O $out_dir/enwiktionary-pages-articles-multistream-index.txt.bz2
